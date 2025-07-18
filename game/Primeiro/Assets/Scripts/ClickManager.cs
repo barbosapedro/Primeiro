11de7e759acf2319db75c11a806cf4ff486827a3
barbosapedro <barbosapedro1343@gmail.com>
2025-07-18 18:36:39 +0100

    Commit 2
    Criar o script para Click Manager, e associar este script ao button.

--- game/Primeiro/Assets/Scripts/ClickManager.cs
+++ game/Primeiro/Assets/Scripts/ClickManager.cs
@@ -1,16 +1,30 @@
+//Reage ao clique do jogador no botao principal e acrescenta ouro. E o nucleo da interaçao ativa
 using UnityEngine;
 
+
+//Unity so permite associar metodos a UI buttons se o script for mono behaviour
 public class ClickManager : MonoBehaviour
 {
+    //Private protege a variavel do acesso direto por outros scripts, e serializefield permite mostrar e editar no Inspector
+    [SerializeField] private double clickValue = 1;
+
+
+    public void OnClick()
+    {
+        GameManager.instance.AddGold(clickValue);
+    }
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
